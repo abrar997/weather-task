ee85e0a7602770a8d924b881a073c64bf249cf2d
abrar997 <abraralrawi997.com>
2024-12-04 07:03:40 +0300

    check updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
-import ghPages from "vite-plugin-gh-pages";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +10,4 @@ export default defineConfig({
     },
   },
   base: "/weather-task/",
-  plugins: [ghPages()],
 });
